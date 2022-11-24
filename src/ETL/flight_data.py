ae73372f900a1df28251253492f2ad963fbb76d3
wvanelteren <wvanelteren@gmail.com>
2022-11-24 20:09:25 +0100

    started on aws glue etl job implementation

--- src/ETL/flight_data.py
+++ src/ETL/flight_data.py
@@ -5,15 +5,18 @@ import awswrangler as wr
 today: date = date.today()
 day: str = today.strftime("%d")
 month: str = today.strftime("%m")
-year: str = today.strftime("Y")
+year: str = today.strftime("%Y")
 
 FLIGHT_DATA_ARRIVALS_PATH: str = (
-    f"S3://wvane.flight-data-raw/arrivals/{year}/{month}/{day}/"  # noqa
+    f"s3://wvane.flight-data-raw/arrivals/{year}/{month}/{day}/"  # noqa
 )
 FLIGHT_DATA_DEPARTURES_PATH: str = (
-    f"S3://wvane.flight-data-raw/arrivals/{year}/{month}/{day}/"  # noqa
+    f"s3://wvane.flight-data-raw/departures/{year}/{month}/{day}/"  # noqa
+)
+
+TARGET_PATH: str = (
+    f"s3://wvane.flight-data-clean/arrivals/{year}/{month}/{day}/"  # noqa
 )
 
 arrivals_df = wr.s3.read_json(path=FLIGHT_DATA_ARRIVALS_PATH, path_suffix=".json")
-
-print(arrivals_df)
+wr.s3.to_csv(arrivals_df, TARGET_PATH, index=False)
---
+++ tests/unit/ETL_jobs/test_fligh_data_etl.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pytest
+
+# from tabulate import tabulate
+
+
+@pytest.fixture
+def load_merged_data():
+    departures_df_1 = pd.read_json("tests/unit/ETL_jobs/departures_data_1.json")
+    departures_df_2 = pd.read_json("tests/unit/ETL_jobs/departures_data_2.json")
+    return pd.concat([departures_df_1, departures_df_2], axis=0, ignore_index=True)
+
+
+def test_if_duplicates_present(load_merged_data):
+    assert load_merged_data["flightID"].is_unique is False
+
+
+def test_name_change(load_merged_data):
+    load_merged_data.rename(columns={"badge_id": "timestamp"}, inplace=True)
+    try:
+        assert "timestamp" in load_merged_data.columns
+    except KeyError:
+        assert False
+
+
+def test_deduplicate_keep_entry_with_latest_timestamp(load_merged_data):
+    df = load_merged_data.sort_values("badge_id").drop_duplicates(
+        ["flightID"], keep="last"
+    )
+    df.to_csv("output_2.csv")
+    assert df["flightID"].is_unique

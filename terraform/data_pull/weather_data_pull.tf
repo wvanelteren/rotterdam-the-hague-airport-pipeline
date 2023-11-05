cea351217879e39892299ef84c82b36675342435
Wouter van Elteren <wvanelteren@gmail.com>
2023-11-05 21:03:02 +0100

    update terraform paths according to folder changes

--- terraform/data_pull/weather_data_pull.tf
+++ terraform/data_pull/weather_data_pull.tf
@@ -1,19 +1,19 @@
 # Create bucket for storing flight data api response
 resource "aws_s3_bucket_acl" "bucket_weather_data_raw" {
-  bucket = "wvane.weather-data-raw"
+  bucket = "wvane.weather-data-raw-${random_uuid.uuid.result}"
   acl    = "private"
 }
 
 # Generates an archive from content, a file, or a directory of files.
 data "archive_file" "zip_fetch_weather_file" {
   type        = "zip"
-  source_file = "${path.root}/../../src/data_pull/weather/fetch_weather.py"
-  output_path = "${path.root}/../../src/data_pull/weather/fetch_weather.zip"
+  source_file = "${path.root}/../../src/data_pull/lambdas/fetch_weather.py"
+  output_path = "${path.root}/../../src/data_pull/lambdas/fetch_weather.zip"
 }
 
 # Create lambda function
 resource "aws_lambda_function" "terraform_pull_weather_data_lambda_func" {
-  filename      = "${path.root}/../../src/data_pull/weather/fetch_weather.zip"
+  filename      = "${path.root}/../../src/data_pull/lambdas/fetch_weather.zip"
   function_name = "weather-data-raw-to-s3"
   role          = aws_iam_role.lambda_role.arn
   handler       = "fetch_weather.lambda_handler"
@@ -21,7 +21,8 @@ resource "aws_lambda_function" "terraform_pull_weather_data_lambda_func" {
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
   environment {
     variables = {
-      BUCKET_NAME = aws_s3_bucket.bucket_weather_data_raw.bucket
+      BUCKET_NAME = aws_s3_bucket.bucket_weather_data_raw.bucket,
+      OPENWEATHER_API_KEY     = var.api_key
     }
   }
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -8,11 +8,17 @@ variable "aws_region" {
 variable "alert_email_id" {
   description = "Email id to send alerts to "
   type        = string
-  default     = "wvanelteren@gmail.com"
+  default     = "example@example.com"
 }
 
 variable "repo_url" {
   description = "Repository url to clone into production machine"
   type        = string
   default     = "https://github.com/wvanelteren/rotterdam-the-hague-airport-pipeline.git"
+}
+
+variable "api_key" {
+  description = "The API key for OpenWeather"
+  type        = string
+  default     = "ENTER API KEY HERE"
 }
